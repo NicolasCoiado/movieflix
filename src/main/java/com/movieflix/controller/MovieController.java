00e989b6f36f31087e54e3306b96312976733938
NicolasCoiado <nicolasmcoiado@gmail.com>
2025-08-04 15:10:40 -0300

    feat: Implements movie registration and movie listing

---
+++ src/main/java/com/movieflix/controller/MovieController.java
@@ -0,0 +1,31 @@
+package com.movieflix.controller;
+
+import com.movieflix.controller.request.MovieRequest;
+import com.movieflix.controller.response.MovieResponse;
+import com.movieflix.entity.Movie;
+import com.movieflix.mapper.MovieMapper;
+import com.movieflix.service.MovieService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("movieflix/movies")
+@RequiredArgsConstructor
+public class MovieController {
+    private final MovieService movieService;
+
+    @PostMapping
+    public ResponseEntity<MovieResponse> save (@RequestBody MovieRequest request){
+        Movie savedMovie = movieService.save(MovieMapper.toMovie(request));
+        return ResponseEntity.status(HttpStatus.CREATED).body(MovieMapper.toMovieResponse(savedMovie));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<MovieResponse>> findAll (){
+        return ResponseEntity.ok(movieService.findAll().stream().map(MovieMapper::toMovieResponse).toList());
+    }
+}
